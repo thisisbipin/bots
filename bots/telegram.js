b2c11f26be51d1852068239363f010913e79063d
thisisbipin <bipinmjadav087@gmail.com>
2022-03-02 09:08:46 +0530

    resolving bug

--- bots/telegram.js
+++ bots/telegram.js
@@ -3,21 +3,25 @@ export function TELEGRAM__BOT(app, axios) {
   dotenv.config();
   const { TELEGRAM_BOT_TOKEN, SERVER_URL } = process.env;
   const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`;
-  const URI = `/webhook/${TELEGRAM_BOT_TOKEN}`;
+  const URI = `/webhook/`;
   const WEBHOOK_URL = SERVER_URL + URI;
-
+  console.log(WEBHOOK_URL);
   const init = async () => {
     const res = await axios.get(
       `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`
     );
-    // console.log(res.data);
+    console.log("=> Telegram : ", res.data);
     console.log("=> Our Telegram bot is ready to go..");
   };
   app.get("/telegram", (req, res) => {
     res.send("Telegram is running...");
   });
+  app.get(URI, (req, res) => {
+    res.send("Telegram Webhook is running...");
+  });
   app.post(URI, async (req, res) => {
     // console.log(req.body);
+    console.log(`=> Telegram : POST message --> ${req.body.message.text}`);
     const chatId = req.body.message.chat.id;
     let text = req.body.message.text;
     if (text == "hi") text = "Hi Akansha padhai karle thoda";

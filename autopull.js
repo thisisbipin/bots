2a6e8f003badee9cf156e084f5955d00c2e07210
thisisbipin <bipinmjadav087@gmail.com>
2022-03-17 19:57:52 +0530

    checking the autopull

--- autopull.js
+++ autopull.js
@@ -1,18 +1,18 @@
 import dotenv from "dotenv";
 import { exec } from "child_process";
 export function set_webHook_reciever(app) {
-    dotenv.config();
-    app.get('/' + process.env.WEBHOOK_URL, (req, res) => {
-        res.send("Working..");
-    });
-    app.post('/' + process.env.WEBHOOK_URL, (req, res) => {
-        console.log('trying to perform post');
-        console.log(req.body);
-        exec("git pull origin master", (error, stdout, stderr) => {
-            console.log(`=> GIT status: ${ stdout }`);
-        });
-        res.send("OK");
-    });
+	dotenv.config();
+	app.get("/" + process.env.WEBHOOK_URL, (req, res) => {
+		res.send("Working..");
+	});
+	app.post("/" + process.env.WEBHOOK_URL, (req, res) => {
+		console.log("trying to perform post");
+		console.log(req.body);
+		exec("git pull origin master && refresh", (error, stdout, stderr) => {
+			console.log(`=> GIT status: ${stdout}`);
+		});
+		res.send("OK");
+	});
 
-    console.log("=> webhook Reciever was set");
+	console.log("=> webhook Reciever was set");
 }
